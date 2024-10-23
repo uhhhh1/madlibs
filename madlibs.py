0c05a3d8773c3e0bb50008c690aa3ceff49e482d
chloe <chloep2838@tuckahoeschools.org>
2024-10-23 12:30:59 +0000

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,18 @@
+adjective1 = input("Type an adjective")
+noun1 = input("Type a noun")
+noun2 = input("type a noun")
+emotion1 = input("type an emotion")
+deadlyillness1 = input("type a deadly illness")
+organ1 = input("type an organ")
+color1 = input("type a color")
+noun3 = input ("type a noun")
+noun4 = input("type a name for the character")
+noun5 = input ("type a noun")
+place1 = input("type a place")
+print(f"""Once upon a time there was a {adjective1}{noun1}
+a hawk came up to it and said, how much hawk would a hawk hawk tuah if a hawk could hawk tuah?, hearing the hawk made it {emotion1}
+after talking to the hawk it went to {place1} to get water, but it was STILL WATER. the still water's noradrenaline made it contract {deadlyillness1}
+{noun4} needed chemotherapy to get rid of it, {noun4}'s {organ1} shut down, and  {noun4} died, but came back as a {noun2}
+, the {noun2} had 1 MILLION BEERS, then gambled all of it on {color1} and lost it all.
+after losing all 1 MILLION BEERS, {noun4} became depressed, and homeless, and had to live in a {noun3}
+{noun5}... the end.""")
